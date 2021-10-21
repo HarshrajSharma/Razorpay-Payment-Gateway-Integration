91cde19ca9d2ba24d0410245928307c2f9ac31e8
Harsh Sharma <harshrajsharma.2003@gmail.com>
2021-10-21 20:27:43 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
+import React, {useState} from "react"
 import logo from './logo.svg';
 import './App.css';
 
+//The script loader function 
+function loadScript(src) {
+	return new Promise((resolve) => {
+		const script = document.createElement('script')
+		script.src = src
+		script.onload = () => {
+			resolve(true)
+		}
+		script.onerror = () => {
+			resolve(false)
+		}
+		document.body.appendChild(script)
+	})
+}
+
+
+
 function App() {
+
+  const [name, setName] = useState('Anonymous')
+
+  const __DEV__ = document.domain === 'localhost' //This will 
+
+  async function displayRazorpay(){
+    const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')  //This is the razorpay sdk
+
+		if (!res) {
+			alert('Razorpay SDK failed to load. Are you online?')
+			return
+		}
+
+		const data = await fetch('http://localhost:5000/razorpay', { method: 'POST' }).then((t) =>
+			t.json()
+		)
+
+		console.log(data)
+
+		const options = {
+			key: __DEV__ ? 'rzp_test_74t567Z8QkYOFB' : 'PRODUCTION_KEY',  
+			currency: data.currency,
+			amount: data.amount.toString(),
+			order_id: data.id,
+			name: 'Donation',
+			description: 'Thank you for nothing. Please give us some money',
+			image: 'http://localhost:5000/logo.svg',
+			handler: function (response) {
+				alert(response.razorpay_payment_id)
+				alert(response.razorpay_order_id)
+				alert(response.razorpay_signature)
+			},
+			prefill: {
+				name,
+				email: 'sdfdsjfh2@ndsfdf.com',
+				phone_number: '9899999999'
+			}
+		}
+		const paymentObject = new window.Razorpay(options)
+		paymentObject.open()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,11 +71,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          onClick={displayRazorpay}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Donate $5
         </a>
       </header>
     </div>
